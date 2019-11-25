77d6d7f3b98e59f459ce3bae553c83d6b1d32249
Elodie Lefevre <elefevre@la-providence.net>
2019-11-25 16:22:45 +0100

    test langlace

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<?php echo "coucou php " ; ?> 
+
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        coucou html Version 2
+        
+        <h1 >coucou html Version 2</h1>
+      
+        <span class="ClassBleu">je suis une classe bleu</span>
+        <span ID="rose">je suis un id rose</span>
+        <h2>PHP</h2>
+        <?php echo "coucou php " ; 
+            
+        ?> 
     </body>
 </html>
